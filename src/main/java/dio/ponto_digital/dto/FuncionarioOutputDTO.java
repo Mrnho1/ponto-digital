99bf6b3d7e6c214706ac43de981cd993c2d32812
lucas <ls.marinho36@gmail.com>
2025-05-21 22:07:47 -0300

    Create FuncionarioOutputDTO

---
+++ src/main/java/dio/ponto_digital/dto/FuncionarioOutputDTO.java
@@ -0,0 +1,12 @@
+package dio.ponto_digital.dto;
+
+import lombok.Data;
+
+@Data
+public class FuncionarioOutputDTO {
+    private Long id;
+    private String nome;
+    private String cpf;
+    private String email;
+    private String telefone;
+}
