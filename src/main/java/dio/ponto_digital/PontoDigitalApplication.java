cdda3e7ec754f93175da2c175e09103ce747854c
lucas <ls.marinho36@gmail.com>
2025-05-21 22:02:49 -0300

    Initial commit

---
+++ src/main/java/dio/ponto_digital/PontoDigitalApplication.java
@@ -0,0 +1,13 @@
+package dio.ponto_digital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontoDigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontoDigitalApplication.class, args);
+	}
+
+}
